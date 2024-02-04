65cbbec4ce9105458c72d60e235fb34e5333a881
Nilo Freitas <nilo.n2m15@gmail.com>
2024-02-04 18:08:56 -0300

    commit inicial

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
+import { useCallback } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import { useFonts } from 'expo-font';
+
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Poppins-Bold': require('./src/assets/Fonts/Poppins-Bold.ttf'),
+    'Poppins-Medium': require('./src/assets/Fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('./src/assets/Fonts/Poppins-Regular.ttf'),
+    'Poppins-Light': require('./src/assets/Fonts/Poppins-Light.ttf'),
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  SplashScreen.hideAsync()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} >
+      <Text style={{ fontFamily: 'Poppins-Bold' }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +34,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    marginTop: Platform.OS === 'ios' ? 70 : 10
+  }
+})
